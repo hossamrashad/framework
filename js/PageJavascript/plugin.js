6e36ad2fdcd7749380947d69e0761f737b673a75
Hossam Rashad <bibo2010508@yahoo.com>
2021-02-14 00:36:25 +0200

    I am Try To Do

--- js/PageJavascript/plugin.js
+++ js/PageJavascript/plugin.js
@@ -1,1 +1,8 @@
-/*globel console, document, window, alert, prompt, confirm */
+/*globel console, document, window, alert, prompt, confirm,$ */
+$("#iFrame").contents().find("#someDiv").removeClass("hidden");
+
+$(document).ready(function () {
+    $('#frameID').load(function () {
+        $('#frameID').contents().find('body').html('Hey, i`ve changed content of <body>! Yay!!!');
+    });
+});
